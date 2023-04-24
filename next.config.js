d49aca0830bf9df00faeffb46ca22428faad5dc7
houariDz95 <houarieddrief7@gmail.com>
2023-04-24 23:21:14 +0100

    pdating

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  functions: {
-    "api/**/*.js": {
-      timeout: 60,
-    },
-  },
   images: {
     domains: ['downloads.hindawi.org'],
   },
