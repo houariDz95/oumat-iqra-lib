eea19f52a581dd37f463b1d65ef312cfdca5f158
houariDz95 <houarieddrief7@gmail.com>
2023-04-24 23:49:59 +0100

    new updating

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,10 @@
 import React  from 'react';
-import Head from 'next/head';
 import '@/styles/globals.css'
 import Layout from '@/components/layout'
 
 export default function App({ Component, pageProps }) {
   return(
     <>
-      <Head>
-        <title>My Next.js App</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <html lang="ar" />
-      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
