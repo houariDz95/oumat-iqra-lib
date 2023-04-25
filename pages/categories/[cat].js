49cea676ee19999c089c6875d1d8e6ca84f2e3c8
houariDz95 <houarieddrief7@gmail.com>
2023-04-25 02:16:57 +0100

    changes

--- pages/categories/[cat].js
+++ pages/categories/[cat].js
@@ -3,8 +3,23 @@ import Feed from '@/components/Feed'
 import { fetchFromAPI } from '@/utils/fetchData';
 import Footer from '@/components/Footer';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
-const  Home = ({books, isLoading}) => {
+const  Home = () => {
+  const [books, setBooks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter()
+  const { cat } = router.query
+    const fetchData = async () => {
+      const data = await fetchFromAPI(`categories/${cat}`);
+      setIsLoading(true)
+      setBooks(data)
+      setIsLoading(false)
+    }
+  
+  useEffect(() => {
+    fetchData()
+  }, [cat])
   return (
     <main className="flex w-screen md:max-h-[90vh] h-full flex-col md:flex-row-reverse">
       <Sidebar  />
@@ -16,24 +31,5 @@ const  Home = ({books, isLoading}) => {
   )   
 }
 
-export async function getServerSideProps(context) {
-  const { params } = context;
-  const { cat } = params;
-  let isLoading = true;
-  try {
-    const data = await fetchFromAPI(`categories/${cat}`);
-    isLoading = false
-
-    return {
-      props: { books: data, isLoading },
-    };
-  } catch (error) {
-    return {
-      props: { data: null },
-    };
-  }finally{
-    isLoading = false
-  }
-}
 
 export default Home
