f8547ff6b0359ea4ea6942599a7278120d7c3cd0
houariDz95 <houarieddrief7@gmail.com>
2023-04-25 13:30:00 +0100

    config vercel

--- pages/categories/[cat].js
+++ pages/categories/[cat].js
@@ -2,24 +2,10 @@ import Sidebar from '@/components/Sidebar'
 import Feed from '@/components/Feed'
 import { fetchFromAPI } from '@/utils/fetchData';
 import Footer from '@/components/Footer';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
 
-const  Home = () => {
-  const [books, setBooks] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const router = useRouter()
-  const { cat } = router.query
-    const fetchData = async () => {
-      const data = await fetchFromAPI(`categories/${cat}`);
-      setIsLoading(true)
-      setBooks(data)
-      setIsLoading(false)
-    }
-  
-  useEffect(() => {
-    fetchData()
-  }, [cat])
+
+const  Categories = ({books, isLoading}) => {
+  console.log(books)
   return (
     <main className="flex w-screen md:max-h-[90vh] h-full flex-col md:flex-row-reverse">
       <Sidebar  />
@@ -31,5 +17,23 @@ const  Home = () => {
   )   
 }
 
+export async function getServerSideProps(context) {
+  let isLoading = true;
+  const { cat } = context.params;
+  try {
+    const data = await fetchFromAPI(`categories/${cat}`);
+    isLoading = false
+    return {
+      props: { books: data, isLoading },
+    };
+  } catch (error) {
+    return {
+      props: { data: null },
+    };
+  }finally{
+    isLoading = false
+  }
+}
 
-export default Home
+
+export default Categories
