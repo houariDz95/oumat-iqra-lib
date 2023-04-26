208a9867958ad97a2a4a43ab206a87434699471e
unknown <houarieddrief7@gmail.com>
2023-04-26 18:37:52 +0200

    new commit

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
 
-const BASE_URL = "https://arabic-books-library.p.rapidapi.com"
-const options = {
-  headers: {
-    'X-RapidAPI-Key': process.env.API_KEY,
-    'X-RapidAPI-Host': 'arabic-books-library.p.rapidapi.com'
-  },
-};
+const BASE_URL = "https://arabic-books-library.p.rapidapi.com";
 
 export const fetchFromAPI = async (url) => {
-  try{
-    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-
-    return data 
-  }catch(err){
-    console.log(err);
+  try {
+    const { data } = await axios.get('/api/config');
+    const options = {
+      headers: {
+        'X-RapidAPI-Key': data.apiKey,
+        'X-RapidAPI-Host': 'arabic-books-library.p.rapidapi.com'
+      },
+    };
+    const response = await axios.get(`${BASE_URL}/${url}`, options);
+    return response.data;
+  } catch (error) {
+    console.error(error);
   }
 };
